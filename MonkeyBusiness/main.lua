3059f71b2b66c2a20712f1520605c7717d4a14d4
Ratakor <ratakor@disroot.org>
2025-06-18 23:14:54 +0200

    Add Monkey Business music pack

---
+++ MonkeyBusiness/main.lua
@@ -0,0 +1,42 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Monkey Business
+--- MOD_ID: MonkeyBusiness
+--- MOD_AUTHOR: [Bombaflex,Ratakor]
+--- MOD_DESCRIPTION: Adds the Monkey Business music pack to Balatro. https://www.youtube.com/watch?v=V3ps8wvrmxw
+--- PRIORITY: 1
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+SMODS.Sound({
+    key = "music1",
+    path = "music1.ogg",
+    replace = "music1"
+})
+
+SMODS.Sound({
+    key = "music2",
+    path = "music2.ogg",
+    replace = "music2"
+})
+
+SMODS.Sound({
+    key = "music3",
+    path = "music3.ogg",
+    replace = "music3"
+})
+
+SMODS.Sound({
+    key = "music4",
+    path = "music4.ogg",
+    replace = "music4"
+})
+
+SMODS.Sound({
+    key = "music5",
+    path = "music5.ogg",
+    replace = "music5"
+})
+
+----------------------------------------------
+------------MOD CODE END----------------------
